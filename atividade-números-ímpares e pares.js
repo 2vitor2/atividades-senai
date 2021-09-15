01697189396e1de8d485f8e5ec896256be3c6f9a
2vitor2 <2gastplayer2@gmail.com>
2021-09-14 21:45:34 -0300

    salvando minhas lições

---
+++ atividade-números-ímpares e pares.js
@@ -0,0 +1,14 @@
+for (let numAtual = 0; numAtual<=14; numAtual++){
+    if (numAtual === 0){
+        
+        console.log('O número é zero.'); 
+
+    }else if (numAtual%2 == 0){
+        
+        console.log('O número ' +numAtual+ ' é par.');
+        
+    }else{
+            
+        console.log('O número ' +numAtual+ ' é ímpar.');
+    }
+}
